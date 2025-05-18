2d8b30c9bffa514583eee94bb7a05186c4355fec
Dhewi Sakti A <dhewisaktianggraeni@students.amikom.ac.id>
2025-05-19 01:02:29 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server berjalan di http://localhost:${port}`);
